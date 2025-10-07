f4124a8bd98eadde497b7b54627abb593e72dad5
hien1172004 <hien1172004@gmail.com>
2025-10-07 19:20:33 +0700

    Initial commit - base microservices structure

---
+++ gateway-service/src/test/java/backend/example/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.example.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ indentity-service/src/test/java/backend/example/indentityservice/IndentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.example.indentityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndentityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
